3a9c1216b2909a594868b429c1ec4a47f441705a
chandrashekhar-meshram <shekharonlinework@gmail.com>
2022-08-30 07:24:45 +0530

    1st change

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,27 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+// We have removed some lines of code from App.js and index.js.
+
+// Render the html tag given in the app.js file into dom using ReactDOM.render.
+
+// Make App component a default export
+
+// Make App component a functional component
+
+// Render the App component on the DOM element with id="root" using ReactDOM.render. Make this change in index.js
+
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+
+const App = () => {
+    return (
+        <>
+            <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+        </>
+    )
+};
+
+export default App;
+
+
+
+
 
